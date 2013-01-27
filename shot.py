4eafedf68d41fd0f10f0c286af499c30c0e16c20
Vostbur <vostbur@gmail.com>
2013-01-27 14:25:39 +0300

    first time profiling

--- shot.py
+++ shot.py
@@ -1,108 +1,179 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
-from __future__ import absolute_import, print_function
-
-import os, getopt
+import os
 import sys
+import json
+import getopt
+from glob import glob
+from codecs import open
+from pyatom import AtomFeed
+from markdown import Markdown
+from datetime import datetime
 from shutil import move, copyfile
 from collections import OrderedDict
-from pyatom import AtomFeed
-from datetime import datetime
+from jinja2 import Environment, FileSystemLoader
 
-from libs.tools import *
-from libs.mdown import Convert
-from libs.templ import Render
-from libs.dump import Dump
+class SelfSyncedDict(dict):
+    '''Dict is saved to file inside with-statement'''
+    def __init__(self, fname, *args, **kwargs):
+        self.fname = fname
+        if os.path.isfile(fname):
+            self.update(self.load())
+        super(SelfSyncedDict, self).__init__(*args, **kwargs)
 
-config = load_config()
-md = Convert()
-tpl = Render(config['templates']['folder'])
+    def load(self):
+        return json.load(open(self.fname, encoding='utf-8'))
 
-def work(the_file, dmp = None):
-    result = md.go(the_file)
-    if result == None:
-        print("File %s has wrong head." % the_file)
-        return None
-    else:
-        result['site'] = config['site']
-        result['author'] = config['author']
-        result['feed'] = config['feed']
-        result['disqus'] = config['disqus']
-        content = tpl.render(config['templates']['article'], result)
-        if dmp != None:
-            save(os.path.join(config['output'], result['date'] + '.html'), content)
-            dmp.archive[result['date'] + '.html'] = result
-        return content
+    def __enter__(self):
+        return self
 
-def archive_page(dmp):
-    result = {}
-    result['site'] = config['site']
-    result['author'] = config['author']
-    result['feed'] = config['feed']
-    result['disqus'] = config['disqus']
-    result['content'] = OrderedDict(sorted(dmp.archive.items(), reverse=True))
-    content = tpl.render(config['templates']['archive'], result)
-    save(os.path.join(config['output'], 'archive.html'), content)
-    idx = os.path.join(config['output'], result['content'].items()[0][0])
-    copyfile(idx, os.path.join(config['output'], 'index.html'))
+    def __exit__(self, *exit_params):
+        json.dump(self, open(self.fname, encoding='utf-8', mode='w'), ensure_ascii=False, indent=4)
 
-def static_page():
-    flist = files(config['nav_pages'])
-    for the_file in flist:
-        content = work(the_file)
-        if content:
-            save(os.path.join(config['output'],\
-                os.path.basename(the_file)[:-3] + '.html'), content)
+class Entry:
+    '''Markdown to HTML'''
+    def __init__(self):
+        self._md = Markdown(extensions = ['extra', 'meta', 'codehilite', 'nl2br'])
+        
+    def convert(self, fname):
+        content = self._md.convert(open(fname, encoding="utf-8").read())
+        meta = self._md.Meta
 
-def rss(dmp):
+        if set(meta.keys()) == set(['date', 'title', 'slug']):
+            return {meta['date'][0]:{
+                                        'title': meta['title'][0], 'slug': meta['slug'][0], 'content': content
+                                    }
+                    }
+
+class Post:
+    '''Render HTML to jinja2 template'''
+    def __init__(self, tpl_folder):
+        self.env = Environment()
+        self.env.loader = FileSystemLoader(os.path.join(tpl_folder, '.'))
+
+    def render(self, tpl, content):
+        return self.env.get_template(tpl).render(content)
+
+def load_config(fname, path=os.getcwd()):
+    config = os.path.join(path, fname)
+    if not os.path.isfile(config):
+        print("Can't find", fname)
+        sys.exit(1)
+    return json.load(open(config, encoding='utf-8'))
+
+def save(fname, content):
+    with open(fname, encoding='utf-8', mode='w') as f:
+        f.write(content)
+
+def rss(index):
     url = config['site'][:-1] if config['site'].endswith('/') else config['site']
     feed = AtomFeed(title=config['title'],
                 feed_url=url + "/feed",
                 url=url,
                 author=config['author'])
-    feed_dict = OrderedDict(sorted(dmp.archive.items(), reverse=True))
-    #current_date = datetime.now().strftime('%Y-%m-%d')
-    try:
-        for x in xrange(10):
-            feed.add(title=feed_dict.items()[x][1]['title'],
-                    content=feed_dict.items()[x][1]['content'],
-                    content_type="html",
-                    author=config['author'],
-                    url=url + feed_dict.items()[x][0],
-                    updated=datetime.strptime(feed_dict.items()[x][1]['date'], '%Y-%m-%d')
-                    )
-    except IndexError:
-        pass
+    for k, v in index.iteritems():
+        feed.add(title=v['title'],
+                content=v['content'],
+                content_type="html",
+                author=config['author'],
+                url=url + k + '.html',
+                updated=datetime.strptime(k, '%Y-%m-%d')
+                )
     save(os.path.join(config['output'], config['feed']), feed.to_string())
 
-def regen_mode(): # режим полной перегенирации файлов
-    clear(config['output'])
-    save('dump.db', '{}')
-    dmp = Dump()
-    flist = files(config['source'])
-    for the_file in flist:
-        work(the_file, dmp)
-    archive_page(dmp)
-    rss(dmp)
-    dmp.sync()
-    static_page()
+def server(dname, port = 8000):
+    try:
+        import SimpleHTTPServer as srvmod
+    except ImportError:
+        import http.server as srvmod
+    try:
+        import SocketServer as socketserver
+    except ImportError:
+        import socketserver
+    os.chdir(dname)
+    Handler = srvmod.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer(("", port), Handler)
+    print("serving at port", port)
+    httpd.serve_forever()
 
-def draft_mode(): # Режим генерации из черновиков
-    dmp = Dump()
-    flist = files(config['draft'])
-    for the_file in flist:
-        if work(the_file, dmp):
-            move(the_file, config['source'])
-    archive_page(dmp)
-    rss(dmp)
-    dmp.sync()
+def usage():
+    print("""Simple Static Pages Generator
+
+Usage: python shot.py {-h,--help | -n,--new | -r,--regen | -s,--server}
+
+  -h, --help    This help.
+  -n, --new     New entry.
+  -r, --regen   Regen all files.
+  -s, --server  Start internal server.
+
+(c) 2013, Alexey Rubtsov""")
+    sys.exit(1)
+
+def new_entry():
+    pattern = 'date: %(field)s\ntitle: %(field)s\nslug: %(field)s\n\n%(field)s\n======' % {'field': current_date}
+    fname = os.path.join(config['source'], current_date + '.md')
+    save(fname, pattern)
+    os.system(config['editor'] % fname)
+
+def static_page():
+    for f in glob(config['nav_pages'] + '/*.md'):
+        page = entry.convert(f)
+        save(os.path.join(config['output'], os.path.basename(f)[:-3] + '.html'),
+             post.render(config['templates']['article'], 
+                dict(config, **page.popitem()[1])))
+
+def initial():
+    def test(param):
+        if param in config:
+            if not os.path.isdir(config[param]):
+                os.makedirs(config[param])
+        else:
+            print("Can't find '%s' in config file." % param)
+            sys.exit(1)
+    for folder in ['source', 'output', 'nav_pages']:
+        test(folder)
+
+def clear(dname, ext='.html'):
+    for the_file in os.listdir(dname):
+        if the_file.endswith(ext):
+            file_path = os.path.join(dname, the_file)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+            except Exception, e:
+                print(e)
+
+def regen():
+    initial()
+    clear(config['output'])
+    if os.path.isfile('dump.json'):
+        os.unlink('dump.json')
+        
+    with SelfSyncedDict('dump.json') as index:
+        for f in glob(config['source'] + '/*.md'):
+            index.update(entry.convert(f))
+            
+    for k, v in index.iteritems():
+        save(os.path.join(config['output'], k + '.html'),
+             post.render(config['templates']['article'], dict(config, **v)))
+
+    index = OrderedDict(sorted(index.items(), reverse=True))
+    save(os.path.join(config['output'], 'archive.html'),
+         post.render(config['templates']['archive'], 
+            dict(config, **{'content': index, 'slug': 'archive'})))
+
+    copyfile(os.path.join(config['output'], index.items()[0][0] + '.html'),
+             os.path.join(config['output'], 'index.html'))
+
+    rss(index)
+    static_page()
 
 def main(args):
     try:
         if not args:
             raise getopt.GetoptError('Error')
-        opt, arg = getopt.getopt(args, 'hirds', ['help', 'init', 'regen', 'draft', 'server'])
+        opt, arg = getopt.getopt(args, 'hnrs', ['help', 'new', 'regen', 'server'])
     except getopt.GetoptError, e:
         usage()
     if arg:
@@ -110,16 +181,19 @@ def main(args):
     for o, a in opt:
         if o in ('-h', '--help'):
             usage()
-        elif o in ('-i', '--init'):
-            initial(**config)
+        elif o in ('-n', '--new'):
+            new_entry()
         elif o in ('-r', '--regen'):
-            regen_mode()
-        elif o in ('-d', '--draft'):
-            draft_mode()
+            regen()
         elif o in ('-s', '--server'):
             server(config['output'], config['port'])
         else:
             usage()
+
+config = load_config('config.json')
+entry = Entry()
+post = Post(config['templates']['folder'])
+current_date = datetime.now().strftime('%Y-%m-%d')
 
 if __name__ == '__main__':
     main(sys.argv[1:])
